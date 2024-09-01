1537bc4437c08ee420569fcef0c8fcd6562dc421
pyakovenko <peotr.yakovenko@gmail.com>
2024-09-01 09:22:45 +0200

    feat: estimate the number of servings

--- src/machine/CoffeeMachine.java
+++ src/machine/CoffeeMachine.java
@@ -6,16 +6,36 @@ public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int availableWater = scanner.nextInt();
+        
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int availableMilk = scanner.nextInt();
+        
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int availableBeans = scanner.nextInt();
+        
         System.out.println("Write how many cups of coffee you will need:");
-        int cups = scanner.nextInt();
+        int requestedCups = scanner.nextInt();
         
-        int water = cups * 200;
-        int milk = cups * 50;
-        int beans = cups * 15;
+        int maxCups = calculateMaxCups(availableWater, availableMilk, availableBeans);
         
-        System.out.println("For " + cups + " cups of coffee you will need:");
-        System.out.println(water + " ml of water");
-        System.out.println(milk + " ml of milk");
-        System.out.println(beans + " g of coffee beans");
+        if (maxCups >= requestedCups) {
+            int extraCups = maxCups - requestedCups;
+            if (extraCups == 0) {
+                System.out.println("Yes, I can make that amount of coffee");
+            } else {
+                System.out.println("Yes, I can make that amount of coffee (and even " + extraCups + " more than that)");
+            }
+        } else {
+            System.out.println("No, I can make only " + maxCups + " cup(s) of coffee");
+        }
+    }
+    
+    public static int calculateMaxCups(int water, int milk, int beans) {
+        int maxWater = water / 200;
+        int maxMilk = milk / 50;
+        int maxBeans = beans / 15;
+        return Math.min(Math.min(maxWater, maxMilk), maxBeans);
     }
 }
